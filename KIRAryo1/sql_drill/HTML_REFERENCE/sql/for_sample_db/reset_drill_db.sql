0a5fd67ac9038e43deee3ff11aec2b6680739b78
Ryoichi Kira <r.kira@grooves.co.jp>
2018-04-11 17:30:37 +0900

    add sample database sql

---
+++ KIRAryo1/sql_drill/HTML_REFERENCE/sql/for_sample_db/reset_drill_db.sql
@@ -0,0 +1,8 @@
+USE first_test
+DROP DATABASE first_test;
+CREATE DATABASE first_test;
+USE first_test;
+SOURCE HTML_REFERENCE/sql/for_sample_db/create_for_mysql.sql;
+SOURCE HTML_REFERENCE/sql/for_sample_db/data.sql;
+# SOURCE ~/engineer_training/KIRAryo1/sql_drill/Vim_practice/HTML_REFERENCE/sql/for_sample_db/create_for_mysql.sql;
+# SOURCE ~/engineer_training/KIRAryo1/sql_drill/Vim_practice/HTML_REFERENCE/sql/for_sample_db/data.sql;
