6d8fc5d9f492db179a62e03234cddf82c6e4b6a0
Ryoichi Kira <r.kira@grooves.co.jp>
2018-04-18 17:39:28 +0900

    add all source-codes in section05

---
+++ KIRAryo1/ruby_training/ruby_book/lib/idiom.rb
@@ -0,0 +1,13 @@
+def find_currency(country)
+  currencies = { japan: 'yen', us: 'dollar', india: 'rupee'}
+  currencies[country]
+end
+
+def show_currency(country)
+  if currency = find_currency(country)
+    currency&.upcase
+  end
+end
+
+puts show_currency(:japan)
+puts show_currency(:brazil)
