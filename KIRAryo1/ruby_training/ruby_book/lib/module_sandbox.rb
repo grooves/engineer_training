e9623f644e9ea74eb7e5e18ca1363a1292959e5e
Ryoichi Kira <r.kira@grooves.co.jp>
2018-04-24 13:13:45 +0900

    fix module_sandbox (finish Section08)

--- KIRAryo1/ruby_training/ruby_book/lib/module_sandbox.rb
+++ KIRAryo1/ruby_training/ruby_book/lib/module_sandbox.rb
@@ -1,34 +1,55 @@
-module AwesomeApi
-  @base_url = ''
-  @debug_mode = false
+require_relative './string_shuffle'
 
-  class << self
-    def base_url=(value)
-      @base_url = value
-    end
+using StringShuffle
+puts 'Alice'.shuffle
 
-    def base_url
-      @base_url
-    end
+class User
+  def initialize(name)
+    @name = name
+  end
 
-    def debug_mode=(value)
-      @debug_mode = value
-    end
-
-    def debug_mode
-      @debug_mode
-    end
-
-    # 上ではわかりやすく定義したが、本来は↓の1行で済む
-    # attr_accessor :base_url, :debug_mode
+  def shuffled_name
+    @name.shuffle
   end
 end
 
-AwesomeApi.base_url = 'http://example.com'
-AwesomeApi.debug_mode = true
+user = User.new('Alice')
+puts user.shuffled_name
+puts 'Alice'.shuffle
 
-puts AwesomeApi.base_url
-puts AwesomeApi.debug_mode
+
+
+# module AwesomeApi
+#   @base_url = ''
+#   @debug_mode = false
+#
+#   class << self
+#     def base_url=(value)
+#       @base_url = value
+#     end
+#
+#     def base_url
+#       @base_url
+#     end
+#
+#     def debug_mode=(value)
+#       @debug_mode = value
+#     end
+#
+#     def debug_mode
+#       @debug_mode
+#     end
+#
+#     # 上ではわかりやすく定義したが、本来は↓の1行で済む
+#     # attr_accessor :base_url, :debug_mode
+#   end
+# end
+#
+# AwesomeApi.base_url = 'http://example.com'
+# AwesomeApi.debug_mode = true
+#
+# puts AwesomeApi.base_url
+# puts AwesomeApi.debug_mode
 
 
 
