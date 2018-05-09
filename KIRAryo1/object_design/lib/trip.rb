3fa6d4b63497aa8ef0896f8d583a52eb2a55f122
Ryoichi Kira <r.kira@grooves.co.jp>
2018-05-09 14:39:47 +0900

    ducktype interface 'prepare_trip'

--- KIRAryo1/object_design/lib/trip.rb
+++ KIRAryo1/object_design/lib/trip.rb
@@ -3,26 +3,28 @@ class Trip
 
   def prepare(preparers)
     preparers.each {|preparer|
-      case preparer
-      when Mechanic
-        preparer.prepare_bicycles(bicycles)
-      when TripCoordinator
-        preparer.buy_food(customres)
-      when Driver
-        preparer.gas_up(vehicle)
-        prepare.fill_water_tank(vehicle)
-      end
-    }
+      preparer.prepare_trip(self) }
   end
 end
 
+# All (Preparer)s are duck which responses 'prepare_trip'
 class TripCoordinator
+  def prepare_trip(trip)
+    buy_food(trip.customers)
+  end
+
   def buy_food(customers)
     # ...
   end
 end
 
 class Driver
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
+  end
+
   def gas_up(vehicle)
     # ...
   end
@@ -33,6 +35,11 @@ class Driver
 end
 
 class Mechanic
+  def prepare_trip(trip)
+    trip.bicycles.each { |bicycle|
+      prepare_bicycle(bicycle)}
+  end
+
   def prepare_bicycles(bicycles)
     bicycles.each { |bicycle| preare_bicycyle(bicycle) }
   end
