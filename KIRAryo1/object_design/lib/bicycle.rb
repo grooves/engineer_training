b81bc17373c47256ae8fe62462fb5ea4dfeba38b
Ryoichi Kira <r.kira@grooves.co.jp>
2018-05-10 17:56:21 +0900

    mistaken inheritance Roadbike <= MountainBike

--- KIRAryo1/object_design/lib/bicycle.rb
+++ KIRAryo1/object_design/lib/bicycle.rb
@@ -16,9 +16,29 @@ class Bicycle
   # And some other methods...
 end
 
-puts bike = Bicycle.new(
-        size:       'M',
-        tape_color: 'red')
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
 
-puts bike.size
-puts bike.spares
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+mountain_bike = MountainBike.new(
+  size: 'S', front_shock: 'Manitou', rear_shock: 'Fox')
+
+puts mountain_bike.size
+puts mountain_bike.spares
+
+# puts bike = Bicycle.new(
+#         size:       'M',
+#         tape_color: 'red')
+#
+# puts bike.size
+# puts bike.spares
---
+++ KIRAryo1/object_design/test.rb
@@ -0,0 +1,7 @@
+class User
+  attr_accessor :last_name, :first_name
+end
+
+module UserHelper
+  def full_name(user)
+end
