a2e2ddcb2211a00ed8e8963f43a442b2246d19ba
Ryoichi Kira <r.kira@grooves.co.jp>
2018-05-11 11:24:59 +0900

    hook methodize of 'spares' method

--- KIRAryo1/object_design/lib/bicycle.rb
+++ KIRAryo1/object_design/lib/bicycle.rb
@@ -5,11 +5,22 @@ class Bicycle
     @size       = args[:size]
     @chain      = args[:chain]     || default_chain
     @tire_size  = args[:tire_size] || default_tire_size
+
+    post_initialize(args)
+  end
+
+  def post_initialize(args)
+    nil
   end
 
   def spares
     { tire_size: tire_size,
-      chain:     chain}
+      chain:     chain}.merge(local_spares)
+  end
+
+  # hook method for subclass to override
+  def local_spares
+    {}
   end
 
   def default_chain
@@ -24,13 +35,12 @@ end
 class RoadBike < Bicycle
   attr_reader :tape_color
 
-  def initialize(args)
+  def post_initialize(args)
     @tape_color = args[:tape_color]
-    super(args)
   end
 
-  def spares
-    super.merge({ tape_color: tape_color})
+  def local_spares
+    {tape_color: tape_color}
   end
 
   def default_tire_size
@@ -41,14 +51,13 @@ end
 class MountainBike < Bicycle
   attr_reader :front_shock, :rear_shock
 
-  def initialize(args)
+  def post_initialize(args)
     @front_shock = args[:front_shock]
     @rear_shock  = args[:rear_shock]
-    super(args)
   end
 
-  def spares
-    super.merge(rear_shock: rear_shock)
+  def local_spares
+    {rear_shock: rear_shock}
   end
 
   def default_tire_size
@@ -57,28 +66,36 @@ class MountainBike < Bicycle
 end
 
 class RecumbentBike < Bicycle
+  attr_reader :flag
+
+  def post_initialize(args)
+    @flag = args[:flag]
+  end
+
+  def local_spares
+    {flag: flag}
+  end
+
   def default_chain
     '9-speed'
   end
 
   def default_tire_size
-    raise NotImplementedError,
-      "This #{self.class} cannot respond to:"
+    '28'
   end
 end
 
-# bent = RecumbentBike.new
+bent = RecumbentBike.new(flag: 'tall and orange')
+puts bent.spares
 
 road_bike = RoadBike.new(
   size: 'M', tape_color: 'red')
-puts road_bike.tire_size
-puts road_bike.chain
+puts road_bike.spares
 
 mountain_bike = MountainBike.new(
   size: 'S', front_shock: 'Manitou', rear_shock: 'Fox')
 
-puts mountain_bike.tire_size
-puts road_bike.chain
+puts mountain_bike.spares
 
 # puts bike = Bicycle.new(
 #         size:       'M',
