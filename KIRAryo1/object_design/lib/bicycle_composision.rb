73739ed671efc2f827fe5a8bf23e2da5a4b6b708
Ryoichi Kira <r.kira@grooves.co.jp>
2018-05-14 17:47:48 +0900

    Bicycle is composed by just a 'config Array'

--- KIRAryo1/object_design/lib/bicycle_composision.rb
+++ KIRAryo1/object_design/lib/bicycle_composision.rb
@@ -12,132 +12,68 @@ class Bicycle
 end
 
 require 'forwardable'
-class Parts
+class Part
   extend Forwardable
   def_delegators :@parts, :size, :each
   include Enumerable
-  attr_reader :parts
 
   def initialize(parts)
     @parts = parts
   end
 
   def spares
-    parts.select {|part| part.needs_spare}
+    select {|part| part.needs_spare}
   end
 end
 
-class Part
-  attr_reader :name, :description, :needs_spare
-
-  def initialize(args)
-    @name        = args[:name]
-    @description = args[:description]
-    @needs_spare = args.fetch(:needs_spare, true)
-  end
-  #
-  # # subclass may override
-  # def post_initialize(args)
-  #   nil
-  # end
-  #
-  # # hook method for subclass to override
-  # def local_spares
-  #   {}
-  # end
-  #
-  # def default_chain
-  #   '10-speed'
-  # end
-  #
-  # def default_tire_size
-  #   raise NotImplementedError
-  # end
-end
-
-class RoadBikeParts < Parts
-  attr_reader :tape_color
-
-  def post_initialize(args)
-    @tape_color = args[:tape_color]
+require 'ostruct'
+module PartsFactory
+  def self.build(config, parts_class = Part)
+    parts_class.new(
+      config.collect {|part_config|
+        create_part(part_config)})
   end
 
-  def local_spares
-    {tape_color: tape_color}
-  end
-
-  def default_tire_size
-    '23'
+  def self.create_part(part_config)
+        OpenStruct.new(
+          name:         part_config[0],
+          description:  part_config[1],
+          needs_spare:  part_config.fetch(2, true))
   end
 end
 
-class MountainBikeParts < Parts
-  attr_reader :front_shock, :rear_shock
+road_config =
+  [['chain',       '10-speed'],
+   ['tire_size',   '23'],
+   ['tape_color',  'red']]
 
-  def post_initialize(args)
-    @front_shock = args[:front_shock]
-    @rear_shock  = args[:rear_shock]
-  end
-
-  def local_spares
-    {rear_shock: rear_shock}
-  end
-
-  def default_tire_size
-    '2.1'
-  end
-end
-
-class RecumbentBikeParts < Parts
-  attr_reader :flag
-
-  def post_initialize(args)
-    @flag = args[:flag]
-  end
-
-  def local_spares
-    {flag: flag}
-  end
-
-  def default_chain
-    '9-speed'
-  end
-
-  def default_tire_size
-    '28'
-  end
-end
-
-chain =
-  Part.new(name: 'chain', description: '10-speed')
-
-road_tire =
-  Part.new(name: 'tire_size', description: '23')
-
-tape =
-  Part.new(name: 'tape_color', description: 'red')
-
-mountain_tire =
-  Part.new(name: 'tire_size', description: '2.1')
-
-rear_shock =
-  Part.new(name: 'rear_shock', description: 'Fox')
-
-front_shock =
-  Part.new(name: 'front_shock', description: 'Manitou', needs_spares: false)
-
-road_bike_parts =
-  Parts.new([chain, road_tire, tape])
+mountain_config =
+  [['chain',       '10-speed'],
+   ['tire_size',   '2.1'],
+   ['front_shock', 'Manitou', false],
+   ['rear_shock',  'Fox']]
 
 road_bike =
-  Bicycle.new(size: 'L', parts: road_bike_parts)
+  Bicycle.new(
+    size:  'L',
+    parts: PartsFactory.build(road_config))
 
-puts road_bike.size
-p road_bike.spares
+puts road_bike.spares
 
 mountain_bike =
-  Bicycle.new(size: 'XL', parts: Parts.new([chain, mountain_tire, front_shock, rear_shock]))
+  Bicycle.new(
+    size:  'L',
+    parts: PartsFactory.build(mountain_config))
 
-puts mountain_bike.size
-puts mountain_bike.spares.size
-puts mountain_bike.parts.size
+puts mountain_bike.spares
+
+recumbent_config =
+  [['chain',       '9-speed'],
+   ['tire_size',   '28'],
+   ['flag',        'tail and orange']]
+
+recumbent_bike = Bicycle.new(
+  size: 'L',
+  parts: PartsFactory.build(recumbent_config))
+
+puts recumbent_bike.spares
