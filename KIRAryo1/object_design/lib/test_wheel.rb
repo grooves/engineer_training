f6ff229b24b7bfc51ed23ea0a2ab227419092f6f
Ryoichi Kira <r.kira@grooves.co.jp>
2018-05-15 15:27:13 +0900

    separate Test target Gear object from Wheel objects

--- KIRAryo1/object_design/lib/test_wheel.rb
+++ KIRAryo1/object_design/lib/test_wheel.rb
@@ -16,8 +16,7 @@ class GearTest < MiniTest::Test
     gear = Gear.new(
       chainring: 52,
       cog:       11,
-      rim:       26,
-      tire:     1.5)
+      wheel: Wheel.new(26, 1.5))
 
     assert_in_delta(137.1, gear.gear_inches, 0.01)
   end
